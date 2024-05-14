1ba0bf268e444d9c603c0f119bcfdfb7c3e77a11
Josh Logan <joshlogan103@gmail.com>
2024-05-14 15:06:40 -0400

    add 2 more models and serializers with a manytomany relationship with a through keyword

--- main_app/models.py
+++ main_app/models.py
@@ -9,17 +9,46 @@ MEALS = (
   ('D', 'Dinner')
 )
 
+class Power(models.Model):
+  name = models.CharField(max_length=50)
+  effect = models.CharField(max_length=100)
+  color = models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.name
+  
+class Radiant(models.Model):
+  name = models.CharField(max_length=100)
+  origin = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+
 class Spren(models.Model):
   name = models.CharField(max_length=100)
   type = models.CharField(max_length=40)
   description = models.CharField(max_length=250)
   age = models.IntegerField()
+  powers = models.ManyToManyField(Power)
+  radiants = models.ManyToManyField(Radiant, through = 'Interaction')
 
   def __str__(self):
     return self.name
   
   def fed_for_today(self):
     return self.feeding_set.filter(date = date.today()).count() >= len(MEALS)
+  
+class Interaction(models.Model):
+  spren = models.ForeignKey(Spren, on_delete=models.CASCADE)
+  radiants = models.ForeignKey(Radiant, on_delete=models.CASCADE)
+  description = models.CharField(max_length=100)
+  date = models.DateField('Interaction date')
+
+  def __str__(self):
+    return self.description
+  
+  class META:
+    ordering=['-date']
   
 class Feeding(models.Model):
   date = models.DateField('Feeding Date')
@@ -31,15 +60,7 @@ class Feeding(models.Model):
   spren = models.ForeignKey(Spren, on_delete=models.CASCADE)
   
   def __str__(self):
-    return f"{self.get_meal_display()} on {self.date}"
+    return f"{self.get_meal_display()} on {self.date} for {self.spren}"
   
   class Meta:
     ordering = ['-date']
-
-class Power(models.Model):
-  name = models.CharField(max_length=50)
-  effect = models.CharField(max_length=100)
-  color = models.CharField(max_length=30)
-
-  def __str__(self):
-    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
-from .models import Spren, Feeding, Power
-from .serializers import SprenSerializer, FeedingSerializer, PowerSerializer
+from .models import Spren, Feeding, Power, Radiant, Interaction
+from .serializers import SprenSerializer, FeedingSerializer, PowerSerializer, RadiantSerializer, InteractionSerializer
 
 # Create your views here.
 
@@ -20,6 +20,18 @@ class SprenDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Spren.objects.all()
   serializer_class = SprenSerializer
   lookup_field = 'id'
+
+  def retrieve(self, request, *args, **kwargs):
+    instance = self.get_object()
+    serializer = self.get_serializer(instance, context = {'request': request})
+
+    powers_not_associated = Power.objects.exclude(id__in = instance.powers.all())
+    powers_serializer = PowerSerializer(powers_not_associated, many = True, context = {'request': request})
+
+    return Response({
+      'spren': serializer.data,
+      'powers_not_associated': powers_serializer.data
+    })
 
 class FeedingListCreate(generics.ListCreateAPIView):
   serializer_class = FeedingSerializer
@@ -50,3 +62,50 @@ class PowerDetail(generics.RetrieveUpdateDestroyAPIView):
   serializer_class = PowerSerializer
   lookup_field = 'id'
 
+class AddPowerToSpren(APIView):
+  def post(self, request, spren_id, power_id):
+    spren = Spren.objects.get(id=spren_id)
+    power = Power.objects.get(id=power_id)
+    spren.powers.add(power)
+    return Response ({
+      'message': f"Power of {power.name} added to {spren.name}"
+    })
+  
+class RemovePowerFromSpren(APIView):
+  def delete(self, request, spren_id, power_id):
+    spren = Spren.objects.get(id = spren_id)
+    power = Power.objects.get(id = power_id)
+    spren.powers.remove(power)
+    return Response ({
+      'message': f"Power of {power.name} removed from {spren.name} "
+    })
+
+class RadiantList(generics.ListCreateAPIView):
+  queryset = Radiant.objects.all()
+  serializer_class = RadiantSerializer
+
+class RadiantDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Radiant.objects.all()
+  serializer_class = RadiantSerializer
+  lookup_field = 'id'
+
+class InteractionList(generics.ListCreateAPIView):
+  queryset = Interaction.objects.all()
+  serializer_class = InteractionSerializer
+
+  def get_queryset(self):
+    spren_id = self.kwargs['spren_id']
+    radiant_id = self.kwargs['radiant_id']
+    return Interaction.objects.filter(spren_id = spren_id, radiants_id = radiant_id)
+  
+  def perform_create(self, serializer):
+    spren_id = self.kwargs['spren_id']
+    radiant_id = self.kwargs['radiant_id']
+    spren = Spren.objects.get(id = spren_id)
+    radiants = Radiant.objects.get(id = radiant_id)
+    serializer.save(spren = spren, radiants = radiants)
+
+class InteractionDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Interaction.objects.all()
+  serializer_class = InteractionSerializer
+  lookup_field = 'id'
